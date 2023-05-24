7f5ebc53cd9533405d5c75fa0739cd79070a5733
Scott Steinhorst <scott.steinhorst@gmail.com>
2023-05-24 12:41:56 -0600

    adding put and delete

--- Taskerapi/Controllers/TasksController.cs
+++ Taskerapi/Controllers/TasksController.cs
@@ -46,8 +46,55 @@ namespace TaskAPI.Controllers
             return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
         }
 
-        // Implement other actions for update and delete
+        // PUT: api/tasks/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, Task task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
 
-        // ...
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/tasks/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
     }
 }
